514dfe52927dcc526a7c82a7ba8eeffb222f80b2
haroldcb <haroldcb@hotmail.fr>
2021-08-24 10:07:16 +0200

    Add a FAQ section for SDKs (#6)
    * Add a FAQ section for SDKs
    * Add the groupLabel setup

--- sidebars.js
+++ sidebars.js
@@ -1,15 +1,26 @@
 module.exports = {
-  docs: [
+  configuration: [
     {
       type: 'category',
       label: 'SDKs documentation',
       items: [
-        'installation',
-        'configuration',
-        'usage',
-        'handling-files',
-        'deprecated'
+        'configuration/installation',
+        'configuration/configuration',
+        'configuration/usage',
+        'configuration/handling-files',
+        'configuration/deprecated'
       ],
     },
   ],
+  faq: [
+    {
+      type: 'category',
+      label: 'FAQ',
+      items: [
+        'faq/required_version',
+        'faq/connection',
+        'faq/action_error'
+      ],
+    },
+  ]
 };
